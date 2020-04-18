a73e23942fe5fbffdc05be3067b88ec7493c558e
mohit2308jain <mohitjain5000@gmail.com>
2020-04-18 18:15:00 +0530

    Header and Footer Components Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,13 +17,17 @@ import { MatButtonModule } from '@angular/material/button';
 
 import 'hammerjs';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
